279c16c935d74a281c3bc206f71e7d64bb4de8b0
ipous <pousig@g.crookcountyschools.org>
2021-03-10 19:26:25 -0800

    More Objects

--- Script/objects.js
+++ Script/objects.js
@@ -14,3 +14,21 @@ console.log(object["age"]);
 console.log(object["school"]);
 console.log(object["state"]);
 console.log(object["favFood"]);
+
+var Subjects = {
+    subject1: "Chemistry",
+    subject2: "History",
+    subejct3: "Robotics"
+}
+
+var Friends = {
+    friend1: "Jaiden",
+    friend2: "Wattson"
+}
+
+console.log(Subjects["subejct3"]);
+console.log(Subjects["subject1"]);
+console.log(Subjects["subject2"]);
+
+console.log(Friends["friend1"]);
+console.log(Friends["friend2"]);
