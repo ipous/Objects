6e686eeecee5d87b44d3e7bb0ee12c490f76a7cc
ipous <pousig@g.crookcountyschools.org>
2021-03-10 19:11:17 -0800

    Objects JS

---
+++ Script/objects.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+var object = {
+    firstName: "Ian",
+    lastName: "Pous",
+    age: 50,
+    school: "Crook County Highschool",
+    state: "Oregon",
+    favFood: "Sushi"
+};
+
+console.log(object["firstName"]);
+console.log(object["lastName"]);
+console.log(object["age"]);
+console.log(object["school"]);
+console.log(object["state"]);
+console.log(object["favFood"]);
